b82729f0abdea5524e18cb054119476748e10c63
Le Duc Dung <dungldc6ts42020@gmail.com>
2022-06-07 22:01:41 +0700

    tách nhỏ các component bên client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/client/header/header.component';
+import { FooterComponent } from './layouts/client/footer/footer.component';
+import { SliderCarouselComponent } from './layouts/client/slider-carousel/slider-carousel.component';
+import { MainClientComponent } from './layouts/client/main-client/main-client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SliderCarouselComponent,
+    MainClientComponent
   ],
   imports: [
     BrowserModule,
